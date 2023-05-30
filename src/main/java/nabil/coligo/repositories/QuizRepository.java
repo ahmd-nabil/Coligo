21704ab4353790a4e9b8a13d232c8bc3703e37d6
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-05-30 15:39:06 +0300

    separated Paging logic into PagingService

--- src/main/java/nabil/coligo/repositories/QuizRepository.java
+++ src/main/java/nabil/coligo/repositories/QuizRepository.java
@@ -1,10 +1,13 @@
 package nabil.coligo.repositories;
 
 import nabil.coligo.model.Quiz;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author Ahmed Nabil
  */
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    Page<Quiz> findAllByOrderByDueTo(Pageable pageable);
 }
--- src/main/java/nabil/coligo/services/AnnouncementServiceJpa.java
+++ src/main/java/nabil/coligo/services/AnnouncementServiceJpa.java
@@ -19,27 +19,12 @@ import java.util.Optional;
 public class AnnouncementServiceJpa implements AnnouncementService{
 
     private final AnnouncementRepository announcementRepository;
-
-    private static final Integer DEFAULT_PAGE_NUMBER = 0;
-    private static final Integer DEFAULT_PAGE_SIZE = 20;
     @Override
     public Page<Announcement> findAll(Integer pageNumber, Integer pageSize) {
-        PageRequest pageRequest = buildPageRequest(pageNumber, pageSize);
+        PageRequest pageRequest = PagingService.buildPageRequest(pageNumber, pageSize);
         return announcementRepository.findAllByOrderByCreatedDateDesc(pageRequest);
     }
 
-    private PageRequest buildPageRequest(Integer pageNumber, Integer pageSize) {
-        Integer queryPageNumber = DEFAULT_PAGE_NUMBER;
-        Integer queryPageSize = DEFAULT_PAGE_SIZE;
-        if(pageNumber != null && pageNumber > 0) {
-            queryPageNumber = pageNumber - 1;
-        }
-
-        if(pageSize != null && pageSize > 0) {
-            queryPageSize = pageSize;
-        }
-        return PageRequest.of(queryPageNumber, queryPageSize);
-    }
     @Override
     public Optional<Announcement> findById(Long id) {
         return announcementRepository.findById(id);
