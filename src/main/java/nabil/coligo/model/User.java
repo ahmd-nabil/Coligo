5fa365c9b3da62c1ecc131e741f1e77b204ee60c
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-05-30 12:51:51 +0300

    adding toString to User (avoiding stackoverflow callback while logging)

--- src/main/java/nabil/coligo/model/User.java
+++ src/main/java/nabil/coligo/model/User.java
@@ -68,4 +68,15 @@ public class User {
     public int hashCode() {
         return Objects.hash(username);
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
