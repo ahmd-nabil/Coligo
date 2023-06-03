91cb7594c61d5fc15a05bd13637653cbd2c3105a
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-06-03 23:27:54 +0300

    created QuizController

---
+++ src/main/java/nabil/coligo/controllers/QuizController.java
@@ -0,0 +1,47 @@
+package nabil.coligo.controllers;
+
+import lombok.RequiredArgsConstructor;
+import nabil.coligo.exceptions.QuizNotFoundException;
+import nabil.coligo.model.Quiz;
+import nabil.coligo.services.QuizService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+/**
+ * @author Ahmed Nabil
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/quizzes")
+public class QuizController {
+
+    private final QuizService quizService;
+
+    @GetMapping
+    public Page<Quiz> findAll(@RequestParam(required = false) Integer pageNumber,
+                                        @RequestParam(required = false) Integer pageSize) {
+        return quizService.findAll(pageNumber, pageSize);
+    }
+
+    @PostMapping
+    public ResponseEntity<URI> saveQuiz(@RequestBody Quiz quiz) {
+        Quiz savedQuiz = quizService.save(quiz);
+        return ResponseEntity.created(URI.create("/api/v1/quizzes/" + savedQuiz.getId())).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateQuiz(@PathVariable(name = "id") Long id, @RequestBody Quiz quiz) {
+        quizService.update(id, quiz).orElseThrow(QuizNotFoundException::new);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteQuiz(@PathVariable(name = "id") Long id) {
+        boolean deleted = quizService.delete(id);
+        if(deleted) return ResponseEntity.ok().build();
+        throw new QuizNotFoundException();
+    }
+}
---
+++ src/test/java/nabil/coligo/controllers/QuizControllerTest.java
@@ -0,0 +1,133 @@
+package nabil.coligo.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nabil.coligo.model.Answer;
+import nabil.coligo.model.Question;
+import nabil.coligo.model.Quiz;
+import nabil.coligo.services.QuizService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Ahmed Nabil
+ */
+@WebMvcTest(QuizController.class)
+class QuizControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    QuizService quizService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    Quiz quiz;
+
+    @BeforeEach
+    void setUp() {
+        Answer answer1 = Answer.builder().id(1L)
+                .answer("A1")
+                .build();
+        Answer answer2 = Answer.builder().id(2L)
+                .answer("A2")
+                .build();
+        Answer answer3 = Answer.builder().id(3L)
+                .answer("A3")
+                .build();
+
+        Question question1 = Question.builder().id(1L)
+                .question("q1?")
+                .answers(new HashSet<>(Arrays.asList(answer1, answer2)))
+                .build();
+        Question question2 = Question.builder().id(2L)
+                .question("q2?")
+                .answers(Collections.singleton(answer3))
+                .build();
+        Question question3 = Question.builder().id(3L)
+                .question("What are arrays?")
+                .build();
+
+
+
+        quiz = Quiz.builder().id(1L)
+                .courseName("Algo")
+                .topic("Arrays")
+                .dueTo(LocalDateTime.parse("2023-05-31T01:30:00"))
+                .questions(new HashSet<>(Arrays.asList(question1, question2, question3))).build();
+    }
+
+    @Test
+    void testSaveQuiz() throws Exception {
+        // given
+        given(quizService.save(any())).willReturn(quiz);
+
+        mockMvc.perform(post("/api/v1/quizzes")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(quiz))
+        ).andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
+    }
+
+    @Test
+    void testUpdateQuiz() throws Exception {
+        // given
+        given(quizService.update(any(), any())).willReturn(Optional.of(quiz));
+
+        mockMvc.perform(put("/api/v1/quizzes/1")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(quiz)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testDeleteQuiz() throws Exception {
+        // given
+        given(quizService.delete(any())).willReturn(true);
+
+        mockMvc.perform(delete("/api/v1/quizzes/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testUpdateQuizFails() throws Exception {
+        // given
+        given(quizService.update(any(), any())).willReturn(Optional.empty());
+        mockMvc.perform(put("/api/v1/quizzes/11")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(quiz)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testDeleteQuizFails() throws Exception {
+        // given
+        given(quizService.delete(any())).willReturn(false);
+
+        mockMvc.perform(delete("/api/v1/quizzes/11")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+}
