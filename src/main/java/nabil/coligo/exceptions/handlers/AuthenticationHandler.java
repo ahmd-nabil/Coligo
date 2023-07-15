44889296663b1a6e6ef54d697bc3c3df2028fd37
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-15 06:30:00 +0300

    add AuthenticationHandler for wrong email or password

---
+++ src/main/java/nabil/coligo/exceptions/handlers/AuthenticationHandler.java
@@ -0,0 +1,19 @@
+package nabil.coligo.exceptions.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author Ahmed Nabil
+ */
+@RestControllerAdvice
+public class AuthenticationHandler {
+    @ExceptionHandler({InternalAuthenticationServiceException.class, BadCredentialsException.class})
+    public ResponseEntity<String> emailNotFound(Exception exception) {
+        return new ResponseEntity<>("email or Password not good !", HttpStatus.UNAUTHORIZED);
+    }
+}
