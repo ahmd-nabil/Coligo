687c721c55dbdf84518092f3964ba5c865329e2a
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-08-26 20:52:21 +0300

    refactored to Pageable Interface

--- src/main/java/nabil/coligo/services/QuizServiceJpa.java
+++ src/main/java/nabil/coligo/services/QuizServiceJpa.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import nabil.coligo.model.Quiz;
 import nabil.coligo.repositories.QuizRepository;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +20,8 @@ public class QuizServiceJpa implements QuizService{
     private final QuizRepository quizRepository;
     @Override
     public Page<Quiz> findAll(Integer pageNumber, Integer pageSize) {
-        PageRequest pageRequest = PagingService.buildPageRequest(pageNumber, pageSize);
-        return quizRepository.findAllByOrderByDueTo(pageRequest);
+        Pageable pageable = PagingService.getPageable(pageNumber, pageSize);
+        return quizRepository.findAllByOrderByDueTo(pageable);
     }
 
     @Override
